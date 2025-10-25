8ef7302e870840c33cac92fc8c511b531a1cd69b
martin <vazquezmartin1240@gmail.com>
2025-10-25 13:49:34 -0600

    prubea

--- FLORERIA/urls.py
+++ FLORERIA/urls.py
@@ -7,8 +7,8 @@ from mozilla_django_oidc.views import OIDCAuthenticationRequestView
 
 urlpatterns = [
     path('dadmin/', admin.site.urls),
-    # path('rest/v1/', include('apirest.urls', namespace='apirest')),
-    # path('rest/v1/auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('rest/v1/', include('apirest.urls', namespace='apirest')),
+    path('rest/v1/auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('oidc/authenticate/', OIDCAuthenticationRequestView.as_view(), name='oidc_authentication_init'),
     path('oidc/callback/', CustomOIDCCallbackView.as_view(), name='oidc_authentication_callback'),
 ]

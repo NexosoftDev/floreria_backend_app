5168c63587dc9efbbef340f72d4d67940a31989f
martin <vazquezmartin1240@gmail.com>
2025-10-25 22:46:54 -0600

    prubea

--- FLORERIA/settings.py
+++ FLORERIA/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -140,11 +140,10 @@ WSGI_APPLICATION = 'FLORERIA.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+DATABASE_URL = os.getenv('DATABASE_URL', 'spatialite:///db.spatialite')
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(default=DATABASE_URL)
 }
 
 
